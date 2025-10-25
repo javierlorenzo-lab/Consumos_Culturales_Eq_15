8487099b39982d242c788c947e56542ca617af5f
Javier Lorenzo <javier.lorenzo@bue.edu.ar>
2025-10-25 15:52:04 -0300

    variables sociales

--- Sripts/Codigo limpio.R
+++ Sripts/Codigo limpio.R
@@ -41,8 +41,11 @@ problems(encc)
 # 0 problemas encontrados
 
 #-------------------------------------------------------------------------------------
+#--------------
 
-#Seleccionar  columnas a mantener
+# --- Seleccionar y renombrar columnas de la base ENCC 2022-2023 ---
+# Selección de columnas relevantes
+
 columnas_a_mantener <- c(
   # Datos Sociodemográficos
   "region", "localidad", "genero", "edad", "grupos_edad", "nse_3", "expansor", "ponderador",
@@ -57,7 +60,7 @@ columnas_a_mantener <- c(
   "noti1", "noti3.1", "noti3.2", "noti3.3",
   
   # Lectura
-  "libro1", "libro3.1", "libro3.2", "libro3.3", "libro5","libro11",
+  "libro1", "libro3.1", "libro3.2", "libro3.3", "libro5", "libro11",
   
   # Radio y Podcasts
   "radio1", "radio4.1", "radio4.2", "radio4.3", "radio4.4", "radio8",
@@ -66,80 +69,106 @@ columnas_a_mantener <- c(
   "musica1", "musica3", "musica9", "musica11", "musica12", "musica14",
   
   # Videojuegos
-  "vj1", "vj2", "vj6","vj6.1","vj7",
+  "vj1", "vj2", "vj6", "vj6.1", "vj7",
   
   # Teatro
-  "teatro1", "teatro3", "teatro4","teatro5",
+  "teatro1", "teatro3", "teatro4", "teatro5",
   
   # Patrimonio
-  "pat1", "pat3","pat4",
+  "pat1", "pat3", "pat4",
   "pat7.1", "pat7.2", "pat7.3", "pat7.4", "pat7.5", "pat7.6", "pat7.7", 
   "pat7.8", "pat7.9", "pat7.10", "pat7.11", "pat7.12", "pat7.13", "pat7.14", 
   "pat7.15", "pat7.16", "pat7.17", "pat7.18", "pat7.19",
   
   # Internet y Redes Sociales
-  "int1", "int3", "int4","int5", "int7.4", "int7.5",
+  "int1", "int3", "int4", "int5", "int7.4", "int7.5",
   
   # Formación y Comunidad
-  "forma1_1", # Asumido como la primera opción de FORMA1
-  "comun1_1", # Asumido como la primera opción de COMUN1
-  "comun4",
+  "forma1_1", "comun1_1", "comun4",
   
-  # Uso del Tiempo / Socio (Nivel educativo)
-  "soc13.1"
+  # Bloque SOC (nivel socioeconómico)
+  "soc1_1", "soc1_2", "soc2", "soc3", "soc4",
+  "soc5", "soc6", "soc7", "soc8", "soc9", "soc10",
+  "soc11", "soc12", "soc13.1", "soc15"
 )
 
-# Agregar dinámicamente todas las columnas que comienzan con "soc"
-cols_soc <- names(df_22_23)[str_detect(names(df_22_23), "^soc")]
-columnas_a_mantener <- unique(c(columnas_a_mantener, cols_soc))
-
-
-# 4. Crear el DataFrame final seleccionando SOLO las columnas de interés
+# Seleccionar columnas
 df_tp_final <- encc_22_23_raw %>%
-  select(all_of(columnas_a_mantener)) %>%
-  # Renombrar variables para claridad en el análisis grupal
+  select(any_of(columnas_a_mantener)) %>%
+  
+  # Renombrar columnas principales y bloque SOC
   rename(
-    # Demográficas y Ponderación
+    # --- Datos básicos ---
     PONDERADOR = ponderador,
     NSE = nse_3,
     EXPANSOR = expansor,
     GENERO = genero,
     
-    # Televisión y Plataformas
+    # --- Televisión y plataformas ---
     VE_TV_AIRE_CABLE = tv1,
     FREC_TV_AIRE = tv6.1,
     FREC_TV_CABLE = tv6.2,
     CONSUMO_STREAMING = tv9,
-    FREC_STREAMING = tv13.1, # Frecuencia de plataformas
+    FREC_STREAMING = tv13.1,
     
-    # Cine
+    # --- Cine ---
     FUE_CINE = cine1,
     FREC_CINE = cine3,
     CONTEO_CINE = cine4,
     
-    # Lectura
+    # --- Lectura ---
     LEE_LIBROS = libro1,
-    CONTEO_LIBROS = libro5,
     FREC_LIBROS_PAPEL = libro3.1,
     FREC_LIBROS_DIGITAL = libro3.2,
+    CONTEO_LIBROS = libro5,
     
-    # Radio y Podcasts
+    # --- Radio y Podcasts ---
     ESCUCHA_RADIO = radio1,
     ESCUCHA_PODCASTS = radio8,
     
-    # Música y Teatro
+    # --- Música ---
     ESCUCHA_MUSICA = musica1,
     FREC_MUSICA = musica3,
     FUE_RECITALES = musica9,
+    
+    # --- Teatro ---
+    FUE_TEATRO = teatro1,
+    FREC_TEATRO = teatro3,
     CONTEO_TEATRO = teatro4,
     
-    # Internet
+    # --- Internet ---
     USA_INTERNET = int1,
     BUSCA_CULTURA_ONLINE = int7.4,
     
-    # Nivel Educativo
-    NIVEL_EDUCATIVO = soc13.1
+    # --- Nivel educativo principal ---
+    NIVEL_EDUCATIVO = soc13.1,
+    
+    # --- Bloque SOC detallado ---
+    EDU_ENCUESTADO = soc1_1,
+    EDU_PSH = soc1_2,
+    SITUACION_PSH = soc2,
+    ULTIMA_OCUP_PSH = soc3,
+    COBERTURA_MEDICA_PSH = soc4,
+    BIEN_INTERNET = soc5,
+    BIEN_AUTO = soc6,
+    BIEN_TARJETA = soc7,
+    BIEN_CELULAR = soc8,
+    BIEN_OTRO = soc9,
+    TIPO_VIVIENDA = soc10,
+    DIFICULTADES_HOGAR = soc11,
+    COBERTURA_SALUD = soc12,
+    TAM_HOGAR = soc14,
+    CONDICION_HABITACIONAL = soc15
   )
+
+# Verificar resultado
+names(df_sel)
+
+
+
+
+
+
 
 # Estructura del DataFrame 
 df_tp_final %>% glimpse()
@@ -155,10 +184,6 @@ df_tp_final %>%
   geom_bar() +
   labs(title = "Distribución de la Población por NSE (Ponderado)",
        y = "Población Representada")
-
-
-
-#--------------
 
 
 
